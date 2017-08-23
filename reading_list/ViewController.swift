4afb4d900dfdf31d7038ae688925517244f16946
nttr <nttr@BI-23557.local>
2017-08-23 16:14:37 +0900

    made main vc like memo app and created tableview and cell

--- reading_list/ViewController.swift
+++ reading_list/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    var contentsarray = [String]()
+    @IBOutlet var contentstableview: UITableView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        contentstableview.dataSource = self
+        contentstableview.delegate = self
+        loadcontents()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadcontents()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,8 +31,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    //test
-    //test2
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contentsarray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contentscell")!
+        cell.textLabel?.text = contentsarray[indexPath.row]
+        return cell
+    }
 
+
+    func loadcontents(){
+        
+        let ud = UserDefaults.standard
+        if ud.array(forKey: "memoarray") != nil{
+            
+            contentsarray = ud.array(forKey: "contentsarray") as! [String]
+            contentstableview.reloadData()
+        }
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "toDetail", sender: nil)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetail" {
+            
+//            let detailviewcontroller = segue.destination as! DetailViewController
+//            let selectedindexpath = memotableview.indexPathForSelectedRow!
+//            detailviewcontroller.selectedmemo = memoarray[selectedindexpath.row]
+//            detailviewcontroller.selectedrow = selectedindexpath.row
+            
+        }
+        
+    }
+    
 }
 
